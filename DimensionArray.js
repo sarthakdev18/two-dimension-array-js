1f7c7d261a4b14ba385dabf07c5512f2d41c116b
Sarthak Dev <91304126+sarthakdev18@users.noreply.github.com>
2023-01-22 19:49:31 +0530

    Add files via upload

---
+++ DimensionArray.js
@@ -0,0 +1,23 @@
+function twoDimensionArray(a, b) {
+    let arr = [];
+
+    for (let i = 0; i< a; i++) {
+        for(let j = 0; j< b; j++) {
+            arr[i] = [];
+        }
+    }
+
+    for (let i = 0; i< a; i++) {
+        for(let j = 0; j< b; j++) {
+            arr[i][j] = j;
+        }
+    }
+    return arr;
+}
+
+const x = 2;
+const y = 16;
+
+const result = twoDimensionArray(x, y);
+console.log(result);
+ 
